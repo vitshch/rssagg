8bc5204a83ef51546786a259d0184b5e55052251
vitalish <1n9e9o0!>
2019-07-10 23:56:34 +0300

    Changes for SUBSCRIPTION table

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO SUBSCRIPTION (NAME, LINK, DESCRIPTION)
+VALUES ('OpenNet', 'https://www.opennet.ru/opennews/opennews_all_utf.rss', 'OpenNet news of IT');
+INSERT INTO SUBSCRIPTION (NAME, LINK, DESCRIPTION)
+VALUES ('ITC UA', 'https://itc.ua/feed/', 'ITC UA News of IT');
+INSERT INTO SUBSCRIPTION (NAME, LINK, DESCRIPTION)
+VALUES ('Java World', 'http://www.javaworld.com/index.xml', 'Java World resource about Java');
